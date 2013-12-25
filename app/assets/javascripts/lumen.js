3f2ac5aa88cac875e02c43df575c2dfbc623d3f0
Stephen Reid <postscript07@gmail.com>
2013-12-25 22:35:43 +0000

    mobile enhancements

--- app/assets/javascripts/lumen.js
+++ app/assets/javascripts/lumen.js
@@ -1,5 +1,14 @@
 $(function() {
-  
+
   $("select.chosen").chosen({allow_single_deselect: true});
-  
+
+  $(window).resize(function() {
+    if (document.documentElement.clientWidth < 992) {
+      $('.tabs-left-please').removeClass('tabs-left');
+    } else {
+      $('.tabs-left-please').addClass('tabs-left');
+    }
+  });
+  $(window).resize();
+
 });

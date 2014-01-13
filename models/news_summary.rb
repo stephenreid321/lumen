6a5b2da4c00510ae7eb5ac92656078ea1adb9e39
Stephen Reid <postscript07@gmail.com>
2014-01-13 11:03:11 +0000

    .

--- models/news_summary.rb
+++ models/news_summary.rb
@@ -27,7 +27,7 @@ class NewsSummary
   end
   
   def self.date
-    ENV['NEWSME_SWITCH_HOUR'].to_i > Time.now.hour ? Date.today - 1 : Date.today - 2
+    ENV['NEWSME_SWITCH_HOUR'].to_i >= Time.now.hour ? Date.today - 1 : Date.today - 2
   end
   
   after_save :get_current_digest!

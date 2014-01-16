43e5dd2f959348d7a265a6ce22f2e90ff2cde49e
Stephen Reid <postscript07@gmail.com>
2014-01-16 12:29:06 +0000

    .

--- app/controllers/groups.rb
+++ app/controllers/groups.rb
@@ -32,12 +32,13 @@ Lumen::App.controllers do
     membership_required!
     @from = 1.week.ago.to_date + 7*params[:w].to_i
     @to = Date.today + 7*params[:w].to_i
+    @upcoming = @to + 7
     
     @top_stories = Hash[@group.news_summaries.map { |news_summary| [news_summary, news_summary.top_stories(@from, @to)[0..2]] }]
     @accounts = @group.memberships.where(:created_at.gte => @from).where(:created_at.lt => @to+1).map(&:account).select { |account| account.affiliated && account.picture }
     @conversations = @group.conversations.where(:updated_at.gte => @from).where(:updated_at.lt => @to+1).order_by(:updated_at.desc).select { |conversation| conversation.conversation_posts.count >= 3 }
-    @new_events = @group.events.where(:created_at.gte => @from).where(:created_at.lt => @to+1).where(:start_time.gte => @from).order_by(:start_time.asc)
-    @period_events = @group.events.where(:start_time.gte => @from).where(:start_time.lt => @to+1).order_by(:start_time.asc)
+    @new_events = @group.events.where(:created_at.gte => @from).where(:created_at.lt => @to+1).where(:start_time.gte => @to).order_by(:start_time.asc)
+    @period_events = @group.events.where(:start_time.gte => @to).where(:start_time.lt => @upcoming+1).order_by(:start_time.asc)
            
     if request.xhr?
       partial :'review/review', :locals => {:from => @from, :to => @to, :top_stories => @top_stories, :accounts => @accounts, :conversations => @conversations, :new_events => @new_events, :period_events => @period_events}

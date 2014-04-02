35140f7b8fa1dd5c101e370889d52b5c6abd435c
Stephen Reid <postscript07@gmail.com>
2014-04-02 14:17:56 +0100

    .

--- app/controllers/groups.rb
+++ app/controllers/groups.rb
@@ -44,36 +44,7 @@ Lumen::App.controllers do
     membership_required! if @group.secret?
     erb :'groups/group'
   end
-  
-  get '/groups/:slug/stats' do
-    sign_in_required!
-    @group = Group.find_by(slug: params[:slug]) || not_found
-    @membership = @group.memberships.find_by(account: current_account)    
-    redirect "/groups/#{@group.slug}/request_membership" if !@membership and @group.closed?    
-    membership_required! if @group.secret?
-      
-    @c = {}
-    @cp = {}      
-    @group.conversations.each { |conversation|
-      conversation.conversation_posts.where(:hidden.ne => true).order_by(:created_at.asc).each_with_index { |conversation_post, i|
-        if i == 0
-          @c[conversation_post.account] = [] if !@c[conversation_post.account]
-          @c[conversation_post.account] << conversation_post
-        end
-        @cp[conversation_post.account] = [] if !@cp[conversation_post.account]
-        @cp[conversation_post.account] << conversation_post
-      }
-    }    
     
-    @e = {}
-    @group.events.each { |event|
-      @e[event.account] = [] if !@e[event.account]
-      @e[event.account] << event
-    }    
-    
-    partial :'groups/stats'    
-  end
-  
   get '/groups/:slug/request_membership' do
     @group = Group.find_by(slug: params[:slug]) || not_found
     not_found if @group.secret?
---
+++ app/controllers/stats.rb
@@ -0,0 +1,36 @@
+Lumen::App.controllers do
+
+  get '/groups/:slug/stats' do
+    sign_in_required!
+    @group = Group.find_by(slug: params[:slug]) || not_found
+    @membership = @group.memberships.find_by(account: current_account)    
+    redirect "/groups/#{@group.slug}/request_membership" if !@membership and @group.closed?    
+    membership_required! if @group.secret?
+      
+    @c = {}
+    @cp = {}      
+    @group.conversations.each { |conversation|
+      conversation.conversation_posts.where(:hidden.ne => true).order_by(:created_at.asc).each_with_index { |conversation_post, i|
+        if i == 0
+          @c[conversation_post.account] = [] if !@c[conversation_post.account]
+          @c[conversation_post.account] << conversation_post
+        end
+        @cp[conversation_post.account] = [] if !@cp[conversation_post.account]
+        @cp[conversation_post.account] << conversation_post
+      }
+    }    
+    
+    @e = {}
+    @group.events.each { |event|
+      @e[event.account] = [] if !@e[event.account]
+      @e[event.account] << event
+    }    
+    
+    if request.xhr?
+      partial :'stats/stats'
+    else
+      erb :'groups/stats'
+    end  
+  end
+  
+end

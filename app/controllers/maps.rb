7c4dde11858a5adec665f23a8a3e255f0970e4c7
Stephen Reid <postscript07@gmail.com>
2014-05-21 11:10:49 +0100

    .

--- app/controllers/maps.rb
+++ app/controllers/maps.rb
@@ -1,12 +1,28 @@
 Lumen::App.controllers do
+  
+  get '/map' do
+    sign_in_required!
+    if request.xhr?
+      points = []
+      points += current_account.network.map(&:affiliations).flatten.map(&:organisation).uniq if params[:organisations]
+      points += current_account.network if params[:accounts] or !params[:map_only]
+      if params[:map_only]
+        partial :'maps/map', :locals => {:points => points}
+      else
+        partial :'maps/map_form', :locals => {:points => points}
+      end
+    else    
+      redirect "/?tab=map"
+    end  
+  end   
       
   get '/groups/:slug/map' do
     @group = Group.find_by(slug: params[:slug])
     membership_required! unless @group.open?          
     if request.xhr?
       points = []
-      points += @group.memberships.map(&:account).map(&:affiliations).flatten.map(&:organisation).uniq if params[:organisations] or !params[:map_only]
-      points += @group.memberships.map(&:account) if params[:accounts] or !params[:map_only]
+      points += @group.memberships.map(&:account).map(&:affiliations).flatten.map(&:organisation).uniq if params[:organisations]
+      points += @group.members if params[:accounts] or !params[:map_only]
       if params[:map_only]
         partial :'maps/map', :locals => {:points => points}
       else

97676f088c7382ccbd97356c83256d5a82ffb73a
Stephen Reid <postscript07@gmail.com>
2014-06-24 11:52:13 +0100

    .

--- app/controllers/me.rb
+++ app/controllers/me.rb
@@ -13,7 +13,7 @@ Lumen::App.controllers do
   
   post '/me/edit' do
     sign_in_required!
-    params[:account][:account_tag_ids] = [] if !params[:account][:account_tag_ids]
+    params[:account][:account_tag_ids] = [] if ENV['ACCOUNT_TAGS_PREDEFINED'] and !params[:account][:account_tag_ids]
     @account = current_account    
     if @account.update_attributes(params[:account])      
       flash[:notice] = "<strong>Great!</strong> Your account was updated successfully."

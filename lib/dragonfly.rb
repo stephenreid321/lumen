9aa7fc51356e773721eead1f3598ed157a2e2cdf
Stephen Reid <postscript07@gmail.com>
2014-08-21 17:29:42 +0100

    .

--- lib/dragonfly.rb
+++ lib/dragonfly.rb
@@ -7,7 +7,7 @@ Dragonfly.app.configure do
     if dragonfly_job = DragonflyJob.find_by(signature: job.signature)
       app.datastore.url_for(dragonfly_job.uid)
     else
-      app.server.url_for(job)
+      app.server.url_for(job, :host => ENV['DOMAIN'])
     end
   end
 

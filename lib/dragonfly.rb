482318d53d018e3e73bc2cfbd6e7aae85143992f
Stephen Reid <postscript07@gmail.com>
2014-09-01 12:18:11 +0100

    .

--- lib/dragonfly.rb
+++ lib/dragonfly.rb
@@ -2,6 +2,7 @@ Dragonfly.app.configure do
   plugin :imagemagick
   url_format '/media/:job/:name'    
   datastore :s3, {:bucket_name => ENV['S3_BUCKET_NAME'], :access_key_id => ENV['S3_ACCESS_KEY'], :secret_access_key => ENV['S3_SECRET']}   
+  secret ENV['DRAGONFLY_SECRET']
   
   define_url do |app, job, opts|    
     if dragonfly_job = DragonflyJob.find_by(signature: job.signature)

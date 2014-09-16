14ad8008bef2652fcb0aae5fc9cc95b8cf21c30d
Stephen Reid <postscript07@gmail.com>
2014-09-16 17:57:19 +0100

    .

--- models/page_view.rb
+++ models/page_view.rb
@@ -4,7 +4,11 @@ class PageView
 
   belongs_to :account, index: true
   
-  field :path
+  field :path, :type => String
+  
+  before_validation do
+    errors.add(:path, 'is invalid') if %w{ico ics}.any? { |x| path.ends_with?(x) }
+  end
     
   def self.fields_for_index
     [:account_id, :path, :created_at]

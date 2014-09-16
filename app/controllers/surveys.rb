906224d08835017afa6bb50741611a7ae625a5cc
Stephen Reid <postscript07@gmail.com>
2014-09-16 17:46:05 +0100

    .

--- app/controllers/surveys.rb
+++ app/controllers/surveys.rb
@@ -92,7 +92,7 @@ Lumen::App.controllers do
       @survey.questions.find(k).answers.create(:text => v, :account => current_account)
     }
     flash[:notice] = "Thanks for taking the survey."
-    redirect "/groups/#{@group.slug}/surveys"
+    redirect (current_account.sign_ins.count == 1 ? '/' : "/groups/#{@group.slug}/surveys")
   end 
   
   get '/groups/:slug/surveys/:id/results', :provides => [:html, :csv] do

3565ebabb3a2ede0a474563767a6529112580922
Stephen Reid <postscript07@gmail.com>
2014-12-02 13:40:38 +0000

    .

--- models/plus_one.rb
+++ models/plus_one.rb
@@ -23,6 +23,7 @@ class PlusOne
     mail = Mail.new(
       :to => conversation_post.account.email,
       :from => "#{group.slug} <#{group.email('-noreply')}>",
+      :reply_to => "#{account.name} <#{account.email}>",
       :subject => "#{account.name} +1'd your post in #{conversation_post.conversation.subject}",
       :body => ERB.new(File.read(Padrino.root('app/views/emails/plus_one.erb'))).result(binding)
     )

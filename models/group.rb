e2a99ac3e8009b33466d9850a247c1e49d07c464
Stephen Reid <postscript07@gmail.com>
2015-03-27 21:22:31 +0000

    .

--- models/group.rb
+++ models/group.rb
@@ -231,7 +231,7 @@ You have been granted membership of the '#{self.slug}' group on #{ENV['SITE_NAME
       # is there a better way of accessing the controller context?
       html = open("http://#{ENV['DOMAIN']}/groups/#{group.slug}/digest?from=#{from.to_s(:db)}&to=#{to.to_s(:db)}&for_email=true&h2=#{URI.escape(h2)}&token=#{Account.find_by(admin: true).secret_token}").read
         
-      if html.include?('<h3>')
+      if html.include?('<h3')
         Mail.defaults do
           delivery_method :smtp, group.smtp_settings
         end    

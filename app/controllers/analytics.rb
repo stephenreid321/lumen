cfef0bca49ec7ae54fbf4b8ba2ff476d7bd8c266
Stephen Reid <postscript07@gmail.com>
2015-08-27 14:11:48 +0100

    fix cumulative totals

--- app/controllers/analytics.rb
+++ app/controllers/analytics.rb
@@ -8,6 +8,8 @@ Lumen::App.controllers do
     site_admins_only!      
     @models = [ConversationPost, Account, Event, PageView].select { |model| model.count > 0 }
     @collections = @models.map { |model| model.order_by(:created_at.asc) }            
+    @from = params[:from] ? Date.parse(params[:from]) : 1.month.ago.to_date
+    @to =  params[:to] ? Date.parse(params[:to]) : Date.today
     erb :'analytics/cumulative_totals'
   end
 
@@ -16,6 +18,8 @@ Lumen::App.controllers do
     group_admins_only!      
     @collection_names = [:conversation_posts, :memberships, :events].select { |collection_name| @group.send(collection_name).count > 0 }      
     @collections = @collection_names.map { |collection_name| @group.send(collection_name).order_by(:created_at.asc) }      
+    @from = params[:from] ? Date.parse(params[:from]) : 1.month.ago.to_date
+    @to =  params[:to] ? Date.parse(params[:to]) : Date.today    
     erb :'analytics/cumulative_totals'
   end 
   

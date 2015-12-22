2955468de1dfae6c04f27dc0abe2f3400a743742
Stephen Reid <postscript07@gmail.com>
2015-12-22 18:27:46 +0000

    .

--- app/controllers/analytics.rb
+++ app/controllers/analytics.rb
@@ -1,39 +1,34 @@
 Lumen::App.controllers do
   
+  before do
+    @from = params[:from] ? Date.parse(params[:from]) : 1.week.ago.to_date
+    @to =  params[:to] ? Date.parse(params[:to]) : Date.today
+  end
+  
   get '/analytics' do
     redirect '/analytics/unique_visitors'
   end
 
   get '/analytics/cumulative_totals' do
     site_admins_only!      
-    @models = [ConversationPost, Account, Event].select { |model| model.count > 0 }
-    @collections = @models
-    @from = params[:from] ? Date.parse(params[:from]) : 1.month.ago.to_date
-    @to =  params[:to] ? Date.parse(params[:to]) : Date.today
+    @collections = [ConversationPost, Account, Event]
     erb :'analytics/cumulative_totals'
   end
 
   get '/groups/:slug/analytics' do
     @group = Group.find_by(slug: params[:slug]) || not_found
-    group_admins_only!      
-    @collection_names = [:conversation_posts, :memberships, :events].select { |collection_name| @group.send(collection_name).count > 0 }      
-    @collections = @collection_names.map { |collection_name| @group.send(collection_name) }      
-    @from = params[:from] ? Date.parse(params[:from]) : 1.month.ago.to_date
-    @to =  params[:to] ? Date.parse(params[:to]) : Date.today    
+    group_admins_only!
+    @collections = [@group.conversation_posts, @group.memberships, @group.events]
     erb :'analytics/cumulative_totals'
   end 
   
   get '/analytics/unique_visitors' do
     site_admins_only!  
-    @from = params[:from] ? Date.parse(params[:from]) : 1.month.ago.to_date
-    @to =  params[:to] ? Date.parse(params[:to]) : Date.today        
     erb :'analytics/unique_visitors'    
   end  
   
   get '/analytics/page_views' do
-    site_admins_only!  
-    @from = params[:from] ? Date.parse(params[:from]) : 1.month.ago.to_date
-    @to =  params[:to] ? Date.parse(params[:to]) : Date.today    
+    site_admins_only!
     erb :'analytics/page_views'    
   end
   

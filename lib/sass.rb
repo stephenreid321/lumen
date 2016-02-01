9f6f75388dd1d3d5ec63bcf45291771f7fad295d
Stephen Reid <postscript07@gmail.com>
2016-02-01 10:53:29 +0000

    does sass build in production?

---
+++ lib/sass.rb
@@ -0,0 +1,24 @@
+require 'sass'
+require 'sass/util'
+require 'sass/script'
+
+module Sass::Script::Functions
+  def colors(color)
+    color = color.to_s.gsub('"','')
+    
+    defaults = {
+      'primary' => '#F5D74B',
+      'primary-dark' => '#CDA70D',
+      'secondary' => '#E74C3C',
+      'secondary-dark' => '#CD4435',
+      'secondary-light' => '#F9DFDD',
+      'grey-light' => '#ECF0F1',
+      'grey-mid' => '#D6DBDF'
+    }
+    
+    v = ENV["#{color.underscore.upcase}_COLOR"] || defaults[color]
+    puts color
+    puts "#{"#{color.underscore.upcase}_COLOR"}: #{v}"
+    Sass::Script::Value::String.new(v)
+  end 
+end

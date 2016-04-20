3ff361e2c12904e265c67a37055ab09f37c6588b
postscript07@gmail.com <postscript07@gmail.com>
2016-04-20 14:26:07 +0100

    .

--- app/controllers/config.rb
+++ app/controllers/config.rb
@@ -40,6 +40,7 @@ Lumen::App.controllers do
       :HIDE_SEARCH_MEMBERSHIPS => ['Hides group memberships on profile search results'],
       :HIDE_SEARCH_TAGS => ['Hides tags on profile search results'],
       :HIDE_MAP_FORM => ['Hides map form'],
+      :HIDE_HOMEPAGE_ACTION_BUTTONS => ['Hides homepage action buttons'],
             
       :AFFILIATION_POSITIONS => 'Comma-separated list of acceptable positions e.g. Novice,Intermediate,Master',
       :ACCOUNT_TAGS_PREDEFINED => ['Turns the tagships profile field into a series of checkboxes'],      

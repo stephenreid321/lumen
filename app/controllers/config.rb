0b7374dfa9829204dab5ebc70f029ba2c6214936
postscript07@gmail.com <postscript07@gmail.com>
2016-04-26 10:38:07 +0100

    GROUP_INDEX_CONVERSATION_LIMIT

--- app/controllers/config.rb
+++ app/controllers/config.rb
@@ -65,6 +65,8 @@ Lumen::App.controllers do
       :REPLY_TO_GROUP => ['Sets the reply-to header to the group address'],
       :HIGHLIGHTED_EVENT_LABEL_TEXT => 'Custom label text for highlighted events',
       
+      :GROUP_INDEX_CONVERSATION_LIMIT => 'Shows this many conversations per group on /groups',
+      
       :SHOW_COMPACT_ORG_FILTER => ['Shows the organisation filter on the compact account search form'],
       :SHOW_COMPACT_TAG_FILTER => ['Shows the account tag filter on the compact account search form'],
       :HIDE_COMPACT_SORT => ['Hides the sort field on the compact account search form'],

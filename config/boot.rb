4f6c0c391c58a40c2bde52ef33c4f393a6a396ba
postscript07@gmail.com <postscript07@gmail.com>
2016-05-24 16:38:17 +0100

    .

--- config/boot.rb
+++ config/boot.rb
@@ -11,6 +11,17 @@ require 'net/imap'
 
 Padrino.load!
 
+Dragonfly.app.configure do    
+  plugin :imagemagick
+  url_format '/media/:job/:name'    
+  datastore :s3, {:bucket_name => ENV['S3_BUCKET_NAME'], :access_key_id => ENV['S3_ACCESS_KEY'], :secret_access_key => ENV['S3_SECRET'], :region => ENV['S3_REGION']}
+  secret ENV['DRAGONFLY_SECRET']
+  
+  define_url do |app, job, opts|    
+    app.datastore.url_for((DragonflyJob.find_by(signature: job.signature) || DragonflyJob.create(uid: job.store, signature: job.signature)).uid)
+  end
+end
+
 Delayed::Worker.max_attempts = 1
 Delayed::Worker.destroy_failed_jobs = false
 

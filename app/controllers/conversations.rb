f52dd6d90311163f90b76aa00322db95a5832015
postscript07@gmail.com <postscript07@gmail.com>
2016-06-27 16:27:57 +0100

    likes ticker

--- app/controllers/conversations.rb
+++ app/controllers/conversations.rb
@@ -156,33 +156,7 @@ Lumen::App.controllers do
     flash[:notice] = "The conversation was unmuted."
     redirect "/conversations/#{@conversation.slug}"
   end      
-  
-  get '/conversation_posts/:id/like' do
-    sign_in_required!
-    @conversation_post = ConversationPost.find(params[:id])
-    membership_required!(@conversation_post.conversation.group)
-    @conversation_post.likes.create(account: current_account)
-    if request.xhr?
-      partial :'conversations/like', :locals => {:conversation_post => @conversation_post}
-    else
-      flash[:notice] = "You liked the post by #{@conversation_post.account.name}"
-      redirect "/conversations/#{@conversation_post.conversation.slug}" 
-    end
-  end
     
-  get '/conversation_posts/:id/remove_like' do
-    sign_in_required!
-    @conversation_post = ConversationPost.find(params[:id])
-    membership_required!(@conversation_post.conversation.group)
-    @conversation_post.likes.find_by(account: current_account).destroy
-    if request.xhr?
-      partial :'conversations/like', :locals => {:conversation_post => @conversation_post}
-    else
-      flash[:notice] = 'Your like was removed'
-      redirect "/conversations/#{@conversation_post.conversation.slug}" 
-    end    
-  end 
-  
   get '/conversation_post_bccs/:id/read', :provides => :gif do
     @conversation_post_bcc = ConversationPostBcc.find(params[:id]) || not_found
     @conversation_post_bcc.read_receipt!
@@ -199,14 +173,4 @@ Lumen::App.controllers do
     partial :'accounts/results_compact', :layout => 'modal'
   end
   
-  get '/conversation_posts/:id/likes' do
-    sign_in_required!
-    @conversation_post = ConversationPost.find(params[:id])    
-    redirect "/conversations/#{@conversation_post.conversation.slug}#conversation-post-#{@conversation_post.id}" unless request.xhr?    
-    @accounts = Account.where(:id.in => @conversation_post.likes.pluck(:account_id))
-    @accounts = @accounts.order(:name.asc).per_page(params[:per_page] || 50).page(params[:page])
-    @title = "People who liked this"
-    partial :'accounts/results_compact', :layout => 'modal'    
-  end
-    
 end

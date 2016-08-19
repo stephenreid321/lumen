9358e3783fabe8f62c0ac8c8dca0cfc6924d50de
postscript07@gmail.com <postscript07@gmail.com>
2016-08-19 18:29:21 +0100

    .

--- app/controllers/config.rb
+++ app/controllers/config.rb
@@ -148,7 +148,7 @@ Lumen::App.controllers do
       heroku.config_var.update(ENV['APP_NAME'], Hash[@environment_variables.map { |k,v| [k, params[k]] }])
     else
       Net::SSH.start(ENV['MAIL_SERVER_ADDRESS'], ENV['MAIL_SERVER_USERNAME'], :password => ENV['MAIL_SERVER_PASSWORD']) do  |ssh|
-        ssh.exec("dokku config:set #{ENV['APP_NAME']} #{@environment_variables.map { |k,v| %Q{#{k}="#{params[k]}"} }.join(' ')}")
+        ssh.exec("dokku config:set #{ENV['APP_NAME']} #{@environment_variables.map { |k,v| %Q{#{k}="#{params[k]}"} if params[k] }.compact.join(' ')}")
       end              
     end
     flash[:notice] = "Your config vars were updated. You may have to refresh the page for your changes to take effect."

61f578d199731dea5a3de61cbff688842c15e99f
postscript07@gmail.com <postscript07@gmail.com>
2016-09-07 12:48:44 +0100

    .

--- lib/dragonfly.rb
+++ lib/dragonfly.rb
@@ -5,8 +5,8 @@ Dragonfly.app.configure do
   if Config['S3_BUCKET_NAME']
     datastore :s3, {:bucket_name => Config['S3_BUCKET_NAME'], :access_key_id => Config['S3_ACCESS_KEY'], :secret_access_key => Config['S3_SECRET'], :region => Config['S3_REGION'], :url_scheme => (Config['SSL'] ? 'https' : 'http')}
   else
-    `ln -s /storage #{PADRINO_ROOT}/assets/dragonfly`
-    datastore :file, {:root_path => "#{PADRINO_ROOT}/assets/dragonfly", :server_root => 'dragonfly'}
+    `ln -s /storage #{Padrino.root('app', 'assets', 'dragonfly')}` if Padrino.env == :production
+    datastore :file, {:root_path => Padrino.root('app', 'assets', 'dragonfly'), :server_root => 'dragonfly'}
   end
   
   secret Config['DRAGONFLY_SECRET']

a36c23b4e877e48a571a9bd93efa32598566369d
postscript07@gmail.com <postscript07@gmail.com>
2016-12-07 11:37:17 +0000

    .

--- script/lumen-install.sh
+++ script/lumen-install.sh
@@ -192,7 +192,7 @@ cat <<EOT >> /var/spool/cron/crontabs/root
 0 4 * * * /usr/bin/dokku run $APP_NAME rake cleanup  
 0 8 * * * /usr/bin/dokku run $APP_NAME rake digests:daily  
 0 0 * * 0 /usr/bin/dokku run $APP_NAME rake digests:weekly
-
+0 * * * * chmod -R 777 /var/lib/dokku/data/storage
 EOT
 
 SESSION_SECRET=$(uuidgen)

d7a510606d474589e0343610f2c32af8a59a5b1c
postscript07@gmail.com <postscript07@gmail.com>
2017-03-15 11:36:33 +0000

    .

--- models/sign_in.rb
+++ models/sign_in.rb
@@ -13,13 +13,6 @@ class SignIn
   after_create do
     if account.sign_ins.count == 1
       account.memberships.where(:status => 'pending').each { |membership| membership.update_attribute(:status, 'confirmed') }      
-      if Config['GROUPS_TO_JOIN_ON_FIRST_SIGN_IN'] # deprecated
-        Config['GROUPS_TO_JOIN_ON_FIRST_SIGN_IN'].split(',').map(&:strip).each { |slug|
-          if group = Group.find_by(slug: slug)
-            group.memberships.create :account => account
-          end
-        }
-      end
       Group.where(:join_on_first_sign_in => true).each { |group|
         group.memberships.create :account => account
       }

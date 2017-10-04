762976c0f80cfcd360ea2963a407173185588507
postscript07@gmail.com <postscript07@gmail.com>
2017-10-04 13:02:48 +0100

    .

--- models/membership.rb
+++ models/membership.rb
@@ -71,7 +71,7 @@ class Membership
     if account.sign_ins.count == 0
       password = Account.generate_password(8)
       account.update_attribute(:password, password) 
-      sign_in_details << "Sign in at http://#{Config['DOMAIN']}/sign_in with the email address #{account.email} and the password #{password}"
+      sign_in_details << %Q{Sign in at http://#{Config['DOMAIN']}/sign_in with the email address #{account.email} and the password <div class="password">#{password}</div>}
     else
       sign_in_details << "Check it out at http://#{Config['DOMAIN']}/groups/#{group.slug}."
     end    
